6f8054c8be4c3c7d6ba1534ff1d7f87b238ebff6
Felipe Bolívar <felipe.bolivar2602@gmail.com>
2021-10-27 17:58:00 -0500

    Aqui esta lo de la clase de el dia de hoy

---
+++ js/dom.js
@@ -0,0 +1,23 @@
+//DOM
+//Document Object Model
+//Modelo de objeto del documento
+
+console.log(document.getElementById('tituloNombre'));
+console.log(document.getElementById('nombres'));
+console.log(document.getElementById('tituloApellido'));
+console.log(document.getElementById('apellido'));
+console.log(document.getElementById('tituloTelefono'));
+console.log(document.getElementById('Telefeno'));
+console.log(document.getElementById('tituloCorreo'));
+console.log(document.getElementById('correo'));
+
+console.log(document.getElementsByName('nombres'));
+console.log(document.getElementsByName('apellido'));
+console.log(document.getElementsByName('Telefeno'))
+console.log(document.getElementsByName('correo'))
+
+console.log(document.getElementsByTagName('input'));
+console.log(document.getElementsByTagName('label'));
+console.log(document.getElementsByTagName('form'));
+console.log(document.getElementsByTagName('script'));
+console.log(document.getElementsByTagName('br'));
